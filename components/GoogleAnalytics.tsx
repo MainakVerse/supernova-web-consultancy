5831cbc132d6a50fed992e2c20dc36e22a16d782
MAINAK CHAUDHURI <mainakchaudhuri671@gmail.com>
2025-07-27 10:45:36 +0530

    Create GoogleAnalytics.tsx

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+// components/GoogleAnalytics.tsx
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = "G-0JZWQFN7ZH";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      {/* Load gtag.js */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      {/* Initialize gtag */}
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
